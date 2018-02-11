ccb2716dac70294fd75df2ac6e5a79ae3ebc0092
Raid55 <boutboulnicholas@gmail.com>
2018-02-11 23:22:04 +0000

    fixed small bug with datetime being a string and not an obj

--- models/base_model.py
+++ models/base_model.py
@@ -9,16 +9,18 @@ class BaseModel:
     """
     def __init__(self):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat()
-        self.updated_at = datetime.now().isoformat()
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.now().isoformat()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         tmpDict = self.__dict__
+        tmpDict['created_at'] = tmpDict['created_at'].isoformat()
+        tmpDict['updated_at'] = tmpDict['updated_at'].isoformat()
         tmpDict['__class__'] = self.__class__.__name__
         return tmpDict
