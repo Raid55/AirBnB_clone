864f0de0a097c01cd38155f5de2d70413098380a
Raid55 <boutboulnicholas@gmail.com>
2018-02-11 23:43:24 +0000

    added kwargs to recreate instance from dict of instance

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,17 @@ class BaseModel:
     """
         Base Model
     """
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, val in kwargs.items():
+                if key is not '__class__':
+                    setattr(self, key, val)
+            self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
