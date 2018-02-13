1f6ab624003687c043eb1f6a4a3e2af4fcbe113d
Raid55 <boutboulnicholas@gmail.com>
2018-02-13 01:23:00 +0000

    finished FileStorage class and methods

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Base clas for Hbnb cmd line Proj """
 import uuid
+import models
 from datetime import datetime
 
 class BaseModel:
@@ -10,7 +11,7 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, val in kwargs.items():
-                if key is not '__class__':
+                if key != "__class__":
                     setattr(self, key, val)
             self.created_at = datetime.strptime(self.created_at, '%Y-%m-%dT%H:%M:%S.%f')
             self.updated_at = datetime.strptime(self.updated_at, '%Y-%m-%dT%H:%M:%S.%f')
@@ -18,12 +19,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         tmpDict = self.__dict__
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+""" File storage manager """
+import json
+import os
+from models.base_model import BaseModel
+
+class FileStorage:
+    """ Class that handles serializing and deserializing instances"""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        return FileStorage.__objects
+
+    def new(self, obj):
+        self.__objects.update({'{}.{}'.format(obj.__class__.__name__, obj.id): obj})
+
+    def save(self):
+        tmpDict = {}
+        for key, val in FileStorage.__objects.items():
+            tmpDict[key] = val.to_dict()
+        with open(FileStorage.__file_path, 'w+') as f:
+            json.dump(tmpDict, f)
+
+    def reload(self):
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                FileStorage.__objects = json.load(f)
+            for key, val in FileStorage.__objects.items():
+                FileStorage.__objects[key] = eval(val['__class__'])(**val)
+        except IOError:
+            pass
+        except ValueError:
+            pass
