a76dfb77ea28a947779167d4416c445526f9b72f
Raid55 <boutboulnicholas@gmail.com>
2018-02-13 09:58:49 +0000

    added show command and edited/added a couple fo helper funcs

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ Cmd line entry point """
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
@@ -20,6 +21,21 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
+    def do_show(self, args):
+        'Fetches and displayed stored obj by id, ex. $ show BaseModel 1234-1234-1234'
+        if args == '':
+            print("** class name missing **")
+            return
+        objName = HBNBCommand.parseCheck_ForClass(args)
+        if objName is not None:
+            if len(args.split()) <= 1:
+                print("** instance id missing **")
+                return
+            print(HBNBCommand.parseCheck_ForId(args))
+
+
+        else:
+            print("** class doesn't exist **")
 
     def do_EOF(self, args):
         'Quit command to exit the program'
@@ -29,16 +45,20 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def parseCheck_ForClass(args):
-        'Convert a series of zero or more numbers to an argument tuple'
+        'Looks for class name imported to check against args'
         tmp = None
-        for obj in args.split():
-            try:
-                tmp = HBNBCommand.__ObjList[HBNBCommand.__ObjList.index(obj)]
-            except ValueError:
-                tmp = None
-                pass
+        try:
+            tmp = HBNBCommand.__ObjList[HBNBCommand.__ObjList.index(args.split()[0])]
+        except ValueError:
+            tmp = None
+            pass
         return tmp
-
+    def parseCheck_ForId(args):
+        'checks for Id in objects in storage'
+        for key, val in storage.all().items():
+            if key == args.split()[0] + '.' +args.split()[1]:
+                return val
+        return None
 
 
 if __name__ == '__main__':
